7fc1a819d6cab45004b5c9fc67ef692cf9db2c4e
phillipnicol <43178367+phillipnicol@users.noreply.github.com>
2022-08-23 18:31:05 -0400

    Update cluster.R

--- R/cluster.R
+++ R/cluster.R
@@ -2,9 +2,9 @@
 
 
 
-gbm.cluster <- function(V, se_V, max.cluster=100) {
+gbm.cluster <- function(out, se_V, max.cluster=100) {
   M <- length(out$D)
-
+  V <- out$V
   J <- nrow(V)
   ll.null <- ll
   ll.null <- sum(dnorm(V/se_V,log=TRUE))
@@ -12,65 +12,10 @@ gbm.cluster <- function(V, se_V, max.cluster=100) {
   bics <- rep(NA,max.cluster)
   bics[1] <- log(J)-2*ll.null
 
-  K <- max.cluster
-  fit <- kmeans(V,centers=K)
-  Pi <- table(fit$cluster)/J
-  mu <- fit$centers
-  ll <- 0
-  ll <- vapply(1:J,FUN.VALUE=numeric(1),function(j) {
-    vec <- vapply(1:K,FUN.VALUE=numeric(1),function(k) {
-      log(Pi[k])+sum(dnorm((V[j,]-mu[k,])/se_V[j,],log=TRUE))
-    })
-    c <- max(vec)
-    c+log(sum(exp(vec-c)))
-  })
-  ll <- sum(ll)
-  bics[max.cluster] <- (M*K+K)*log(J)-2*ll
-
-  K <- floor(max.cluster/2)
-  lb <- 1; ub <- max.cluster
-  K.prev <- 1
-  while(TRUE) {
-    print(K)
-    fit <- kmeans(V,centers=K)
-
-    if(K == max.cluster | K==1) {
-      break
-    } else if(abs(lb-ub) <= 1) {
-      break
-    }
-
-
-    Pi <- table(fit$cluster)/J
-    mu <- fit$centers
-    ll <- 0
-    ll <- vapply(1:J,FUN.VALUE=numeric(1),function(j) {
-      vec <- vapply(1:K,FUN.VALUE=numeric(1),function(k) {
-        log(Pi[k])+sum(dnorm((V[j,]-mu[k,])/se_V[j,],log=TRUE))
-      })
-      c <- max(vec)
-      c+log(sum(exp(vec-c)))
-    })
-    ll <- sum(ll)
-    bics[K] <- (M*K+K)*log(J)-2*ll
-    print(bics[K])
-
-    if(bics[K] < bics[ub]) {
-      ub <- K
-      K.new <- floor(mean(c(ub,lb)))
-    } else{
-      lb <- K
-      K.new <- floor(mean(c(ub,lb)))
-    }
-    K <- K.new
-  }
-
-  best <- which.min(bics)
-  fit <- kmeans(V,centers=best)
-
   for(K in 2:max.cluster) {
     print(K)
     fit <- kmeans(V,centers=K)
+
     Pi <- table(fit$cluster)/J
     mu <- fit$centers
     ll <- 0
@@ -85,16 +30,20 @@ gbm.cluster <- function(V, se_V, max.cluster=100) {
     bics[K] <- (M*K+K)*log(J)-2*ll
     print(bics[K])
 
-    if(bics[K] > bics[K-1]) {
+    if(bics[K-1] < bics[K]) {
       break
     }
-    cluster <- fit$cluster
+
+    fit.prev <- fit
+
   }
 
   val <- list()
   val$bic <- bics
-  val$cluster <- cluster
+  val$cluster <- fit.prev$cluster
+  val$fc.centers <- out$U %*% t(fit.prev$centers)
+  A <- matrix(out$alpha,nrow=nrow(out$W),ncol=ncol(val$fc.centers))
+  val$full.centers <- A + val$fc.centers
   return(val)
 }
-
 
