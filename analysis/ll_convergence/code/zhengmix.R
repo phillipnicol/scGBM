d06a8d59aa99263014221b64d6ffceb43694fb0e
Phillip Nicol <pnicol@node12.cm.cluster>
2024-05-20 18:00:17 -0400

    Rerun blish

--- analysis/ll_convergence/code/zhengmix.R
+++ analysis/ll_convergence/code/zhengmix.R
@@ -58,10 +58,10 @@ library(fastglm)
 proj.time <- c()
 proj.ll <- c()
 subset <- sample(1:ncol(Y),size=400,replace=FALSE)
-for(k in seq(25,250,by=25)) {
+for(k in 250) {
   print(k)
   start <- Sys.time()
-  out <- gbm.sc(Y1,M=20,max.iter=k,subset=400,ncores=12,tol=10^{-4})
+  out <- gbm.sc(Y1,M=20,max.iter=k,subset=400,ncores=12,tol=10^{-5})
   end <- Sys.time()
   proj.time <- c(proj.time,difftime(end,start,units="secs")[[1]])
 
