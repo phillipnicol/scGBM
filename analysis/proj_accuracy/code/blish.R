e47ee910c738190568f804197546e5086f4c46e4
Phillip Nicol <philnicol740@gmail.com>
2024-07-31 14:09:13 -0400

    Updating blish code

--- analysis/proj_accuracy/code/blish.R
+++ analysis/proj_accuracy/code/blish.R
@@ -1,7 +1,7 @@
 
 
-library(Seurat)
-Sco <- readRDS("../../data/blish.RDS")
+#library(Seurat)
+#Sco <- readRDS("../../data/blish.RDS")
 
 Y <- readRDS("../../data/blish_counts.RDS")
 Y <- as.matrix(Y)
@@ -22,24 +22,15 @@ true.v <- out$V
 Pv <- out$V %*% t(out$V)
 
 set.seed(1)
-subset <- seq(1000,10000,by=1000)
+subset <- seq(1000,43000,by=1000)
 iters <- 100
 res <- matrix(0, nrow=length(subset), ncol=iters)
 for(i in 1:length(subset)) {
   for(j in 1:iters) {
     out <- gbm.sc(Y,M=20,subset=subset[i],ncores=10)
-    Pv.hat <- out$scores %*% solve(t(out$scores) %*% out$scores) %*% t(out$scores)
+    Pv.hat <- out$scores %*% solve(t(out$scores) %*% out$scores + diag(0.001)) %*% t(out$scores)
     res[i,j] <- sqrt(mean((Pv - Pv.hat)^2))
     print(res[i,j])
+    saveRDS(res, "blish_proj_accuracy.RDS")
   }
 }
-
-
-saveRDS(res, "blish_proj_accuracy.RDS")
-
-
-
-
-
-
-
