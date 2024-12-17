5f2a35026a649ccb6152e817369558524145cd1d
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2024-12-17 10:23:47 -0500

    Updating zhengmix

--- analysis/cluster_accuracy/code/zhengmix.R
+++ analysis/cluster_accuracy/code/zhengmix.R
@@ -24,6 +24,8 @@ Sco <- FindNeighbors(Sco,reduction = "gbm")
 Sco <- FindClusters(Sco)
 gbm <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
+
+
 fit <- glmpca(Y, L=20)
 Sco <- CreateSeuratObject(counts=Y)
 Sco[["glmpca"]] <- CreateDimReducObject(embeddings=as.matrix(fit$res$factors),key="GLMPCA_")
@@ -57,6 +59,27 @@ Sco <- FindClusters(Sco)
 #apr <- adj.rand.index(sce$phenoid, Sco$seurat_clusters)
 apr <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
+
+my.dist <- dist(Sco@reductions$pca@cell.embeddings) |> as.matrix()
+avg.dist <- rep(0, length(unique(phenoid)))
+for(i in 1:length(avg.dist)) {
+  avg.dist[i] <- mean(my.dist[phenoid == unique(phenoid)[i],phenoid == unique(phenoid)[i]])
+}
+
+
+
+embedding <- umap::umap(Sco@reductions$pca@cell.embeddings)
+
+p <- data.frame(x=embedding$layout[,1], y=embedding$layout[,2], color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point() +
+  theme_bw()
+
+p <- data.frame(x=out$scores[,1], y=out$scores[,2], color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point() +
+  theme_bw()
+
 library(fastglm)
 proj_res <- rep(0, 10)
 for(j in 1:10) {
@@ -71,12 +94,26 @@ for(j in 1:10) {
 }
 gbmproj <- mean(proj_res)
 
-results <- c(gbm,gbmproj, l2pca, sct,apr,glmpca)
-names(results) <- c("scGBM-full", "scGBM-proj", "log+scale+PCA", "SCT", "APR","GLM-PCA")
+### LOG-PCA
+Sco <- CreateSeuratObject(counts=Y)
+L <- median(colSums(Y))
+YL <- log(sweep(Y,MARGIN=2,STATS=L^{-1}*colSums(Y),FUN="/") + 1)
+my.pca <- irlba::prcomp_irlba(t(YL), n=20)
+rownames(my.pca$x) <- colnames(Y)
+Sco[["lpca"]] <- CreateDimReducObject(embeddings=my.pca$x,key="LPCA_")
+Sco <- FindNeighbors(Sco,reduction = "lpca")
+Sco <- FindClusters(Sco)
+lpca_noscale <- adj.rand.index(phenoid, Sco$seurat_clusters)
+
+results <- c(gbm,gbmproj, l2pca, sct,apr,glmpca,lpca_noscale)
+names(results) <- c("scGBM-full", "scGBM-proj", "Log+Scale+PCA", "SCT", "APR","GLM-PCA (SGD)",
+                    "Log+PCA")
 
 saveRDS(results, file="../data/zhengmix8eq.RDS")
 
 
+
+##### CLASS SIZE IMBALANCE #####
 
 ### 8uneq
 sizes <- c(100, 100, 600, 100, 100, 100, 100, 100)
@@ -102,7 +139,30 @@ Sco <- FindNeighbors(Sco,reduction = "gbm")
 Sco <- FindClusters(Sco)
 gbm <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
-fit <- glmpca(Y, L=20)
+
+#Umap
+Sco <- RunUMAP(Sco, reduction = "gbm", dims=1:20)
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.scgbm <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("scGBM", face = "bold", size = 14))
+
+
+set.seed(1)
+fit <- glmpca(Y, L=20, minibatch="stochastic", ctl=list(batch_size=400))
 Sco <- CreateSeuratObject(counts=Y)
 Sco[["glmpca"]] <- CreateDimReducObject(embeddings=as.matrix(fit$res$factors),key="GLMPCA_")
 Sco <- FindNeighbors(Sco,reduction = "glmpca")
@@ -110,6 +170,27 @@ Sco <- FindClusters(Sco)
 glmpca <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
 
+Sco <- RunUMAP(Sco, dims=1:20, reduction="glmpca")
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.glmpca <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("GLM-PCA (SGD)", face = "bold", size = 14))
+
+
+##Log+Scale+PCA
 Sco <- CreateSeuratObject(counts=Y)
 Sco <- NormalizeData(Sco)
 Sco <- FindVariableFeatures(Sco,nfeatures=nrow(Y))
@@ -119,7 +200,26 @@ Sco <- FindNeighbors(Sco)
 Sco <- FindClusters(Sco)
 l2pca <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
+Sco <- RunUMAP(Sco, dims=1:20)
 
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.lpca <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("Log+Scale+PCA", face = "bold", size = 14))
+
+##SCTRANSFORM
 Sco <- CreateSeuratObject(counts=Y)
 Sco <- SCTransform(Sco, variable.features.n = nrow(Y))
 Sco <- RunPCA(Sco,npcs=20)
@@ -127,6 +227,26 @@ Sco <- FindNeighbors(Sco)
 Sco <- FindClusters(Sco)
 sct <- adj.rand.index(phenoid, Sco$seurat_clusters)
 
+Sco <- RunUMAP(Sco, dims=1:20)
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.sct <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("SCT+PCA", face = "bold", size = 14))
+
+###APR
 Sco <- CreateSeuratObject(counts=Y)
 Sco <- SCTransform(Sco, vst.flavor = "v1", method="offset",variable.features.n = nrow(Y))
 Sco <- RunPCA(Sco,npcs=20)
@@ -134,6 +254,90 @@ Sco <- FindNeighbors(Sco)
 Sco <- FindClusters(Sco)
 #apr <- adj.rand.index(sce$phenoid, Sco$seurat_clusters)
 apr <- adj.rand.index(phenoid, Sco$seurat_clusters)
+
+
+Sco <- RunUMAP(Sco, dims=1:20)
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.apr <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("APR+PCA", face = "bold", size = 14))
+
+
+
+
+
+##SCTRANSFORM
+Sco <- CreateSeuratObject(counts=Y)
+Sco <- SCTransform(Sco, variable.features.n = nrow(Y))
+Sco <- RunPCA(Sco,npcs=20)
+Sco <- FindNeighbors(Sco)
+Sco <- FindClusters(Sco)
+sct <- adj.rand.index(phenoid, Sco$seurat_clusters)
+
+Sco <- RunUMAP(Sco, dims=1:20)
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.sct <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("SCT+PCA", face = "bold", size = 14))
+
+
+### LOG-PCA
+Sco <- CreateSeuratObject(counts=Y)
+L <- median(colSums(Y))
+YL <- log(sweep(Y,MARGIN=2,STATS=L^{-1}*colSums(Y),FUN="/") + 1)
+my.pca <- irlba::prcomp_irlba(t(YL), n=20)
+rownames(my.pca$x) <- colnames(Y)
+Sco[["lpca"]] <- CreateDimReducObject(embeddings=my.pca$x,key="LPCA_")
+Sco <- FindNeighbors(Sco,reduction = "lpca")
+Sco <- FindClusters(Sco)
+lpca_noscale <- adj.rand.index(phenoid, Sco$seurat_clusters)
+
+Sco <- RunUMAP(Sco, dims=1:20, reduction="lpca")
+
+p1 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=phenoid) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cell type")
+
+p2 <- data.frame(x=Sco@reductions$umap@cell.embeddings[,1],
+                 y=Sco@reductions$umap@cell.embeddings[,2],
+                 color=Sco$seurat_clusters) |>
+  ggplot(aes(x=x,y=y,color=color)) +
+  geom_point(size=0.5) + theme_bw() +
+  xlab("UMAP1") + ylab("UMAP2") + labs(color="Cluster")
+
+p.lpca_ns <- ggarrange(p1, p2, nrow=1) |>
+  annotate_figure(top = text_grob("Log+PCA", face = "bold", size = 14))
+
+
 
 library(fastglm)
 proj_res <- rep(0, 10)
@@ -149,11 +353,25 @@ for(j in 1:10) {
 }
 gbmproj <- mean(proj_res)
 
-results <- c(gbm,gbmproj, l2pca, sct,apr,glmpca)
-names(results) <- c("scGBM-full", "scGBM-proj", "log+scale+PCA", "SCT", "APR","GLM-PCA")
+results <- c(gbm,gbmproj, l2pca, sct,apr,glmpca,lpca_noscale)
+names(results) <- c("scGBM-full", "scGBM-proj", "Log+Scale+PCA", "SCT", "APR","GLM-PCA (SGD)",
+                    "Log+PCA")
 
 saveRDS(results, file="../data/zhengmix8uneq.RDS")
 
 
+
+##Final plot
+
+p <- ggarrange(p.scgbm,
+               p.glmpca,
+               p.lpca,
+               p.sct,
+               p.apr,
+               p.lpca_ns,
+               nrow=6)
+
+
+ggsave(p, filename="../plots/all_umap_subsampled.png", width=9.01, height=5*5, units="in")
 
 
