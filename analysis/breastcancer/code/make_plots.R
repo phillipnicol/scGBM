fface2715f2b668e3535fdfe992268d6255974db
Phillip Nicol <43178367+phillipnicol@users.noreply.github.com>
2025-01-10 10:10:33 -0500

    Updating make plots

--- analysis/breastcancer/code/make_plots.R
+++ analysis/breastcancer/code/make_plots.R
@@ -9,12 +9,13 @@ V <- readRDS("../data/V.RDS")
 p.embedding <- data.frame(x=V[,1], y=V[,2], color=meta$celltype_major) |>
   ggplot(aes(x=x,y=y,color=color)) +
   geom_point(size=0.25) +
-  theme_bw()
+  theme_bw() +
+  xlab("GBM1") + ylab("GBM2")
 
 H.table <- readRDS("../data/H.table.minor.RDS") |> as.data.frame()
 
 
-
+colors <- rainbow(12)
 
 p.heatmap <- pheatmap::pheatmap(H.table,legend=TRUE, color=colorRampPalette(c("white","red"))(100),
                    breaks=seq(0,1,by=0.01),
@@ -22,6 +23,30 @@ p.heatmap <- pheatmap::pheatmap(H.table,legend=TRUE, color=colorRampPalette(c("w
                    colnames=TRUE,
                    cluster_rows = ifelse(nrow(H.table) > 1, TRUE,FALSE),
                    cluster_cols = ifelse(nrow(H.table) > 1, TRUE,FALSE))
+
+library(grid)
+my.colors <- rep("black", unique(meta$celltype_minor) |> length())
+names(my.colors) <- unique(meta$celltype_minor)[p.heatmap$tree_row$order]
+
+
+my.colors["Luminal Progenitors"] <- colors[1]
+my.colors["Myoepithelial"] <- colors[4]
+my.colors["Plasmablasts"] <- colors[7]
+my.colors["Cycling PVL"] <- colors[10]
+
+my.colors["Endothelial Lymphatic LYVE1"] <- colors[2]
+my.colors["Endothelial ACKR1"] <- colors[5]
+my.colors["Endothelial RGS5"] <- colors[8]
+my.colors["Endothelial CXCL12"] <- colors[11]
+
+my.colors["Mature Luminal"] <- colors[3]
+my.colors["Cancer Basal SC"] <- colors[6]
+my.colors["Cancer Cycling"] <- colors[9]
+my.colors["Cancer Her2 SC"] <- colors[12]
+
+p.heatmap$gtable$grobs[[4]]$gp=gpar(col=my.colors)
+p.heatmap$gtable$grobs[[5]]$gp=gpar(col=my.colors)
+
 
 Vu <- readRDS("../data/gbm.umap.RDS")
 
@@ -50,7 +75,6 @@ p <- data.frame(x=Vu[,1], y=Vu[,2], color=ifelse(meta$celltype_minor %in% c("Lum
   geom_point(size=0.25) +
   theme_bw()
 
-colors <- rainbow(12)
 
 p_high_cci_low_icc <- data.frame(x=Vu[,1], y=Vu[,2], color=ifelse(meta$celltype_minor %in% c("Luminal Progenitors",
                                                                             "Myoepithelial",
@@ -61,9 +85,9 @@ p_high_cci_low_icc <- data.frame(x=Vu[,1], y=Vu[,2], color=ifelse(meta$celltype_
   ggplot(aes(x=x,y=y,color=color, alpha=color)) +
   scale_color_manual(values = c(" " = "grey",
                                 "Luminal Progenitors" = colors[1],
-                                "Myoepithelial" = colors[2],
-                                "Plasmablasts" = colors[3],
-                                "Cycling PVL" = colors[4]),
+                                "Myoepithelial" = colors[4],
+                                "Plasmablasts" = colors[7],
+                                "Cycling PVL" = colors[10]),
                      breaks=c("Luminal Progenitors",
                               "Myoepithelial",
                               "Plasmablasts",
@@ -86,10 +110,10 @@ p_high_cci_high_icc <- data.frame(x=Vu[,1], y=Vu[,2], color=ifelse(meta$celltype
                                                  " ")) |>
   ggplot(aes(x=x,y=y,color=color, alpha=color)) +
   scale_color_manual(values = c(" " = "grey",
-                                "Endothelial Lymphatic LYVE1" = colors[5],
-                                "Endothelial ACKR1" = colors[6],
-                                "Endothelial RGS5" = colors[7],
-                                "Endothelial CXCL12" = colors[8]),
+                                "Endothelial Lymphatic LYVE1" = colors[2],
+                                "Endothelial ACKR1" = colors[5],
+                                "Endothelial RGS5" = colors[8],
+                                "Endothelial CXCL12" = colors[11]),
                      breaks=c("Endothelial Lymphatic LYVE1",
                               "Endothelial ACKR1",
                               "Endothelial RGS5",
@@ -114,9 +138,9 @@ p_low_cci <- data.frame(x=Vu[,1], y=Vu[,2], color=ifelse(meta$celltype_minor %in
   ggplot(aes(x=x,y=y,color=color,alpha=color)) +
   geom_point(size=0.25) +
   scale_color_manual(values = c(" " = "grey",
-                                "Mature Luminal" = colors[9],
-                                "Cancer Basal SC" = colors[10],
-                                "Cancer Cycling" = colors[11],
+                                "Mature Luminal" = colors[3],
+                                "Cancer Basal SC" = colors[6],
+                                "Cancer Cycling" = colors[9],
                                 "Cancer Her2 SC" = colors[12]),
                      breaks = c("Mature Luminal",
                                 "Cancer Basal SC",
@@ -145,7 +169,37 @@ p.full <- ggarrange(p.embedding,
 ggsave(p.full, filename="../plots/gbm_umap_cci_breastcancer.png",
        width=10.3, height=16, units="in")
 
+#Editable for illustrator
+#ggsave(p.full, filename="../plots/gbm_umap_cci_breastcancer.pdf",
+#       width=10.3, height=16, units="in")
 
+
+## Look into Cycling PVL
+
+p.list <- list()
+for(i in 1:5) {
+  p <- data.frame(x=V[,2*i-1], y=V[,2*i], color=ifelse(meta$celltype_minor == "Cycling PVL", "Cycling PVL", "Other")) |>
+    arrange(color=="Cycling PVL") |>
+    ggplot(aes(x=x,y=y,color=color)) +
+    geom_point(size=0.5) +
+    scale_color_manual(labels=c("Cycling PVL", "Other"), values=c("firebrick", "grey90")) +
+    theme_bw() +
+    labs(color="") +
+    xlab(paste0("GBM", 2*i - 1)) + ylab(paste0("GBM", 2*i))
+
+  p.list[[i]] <- p
+}
+
+p <- ggarrange(p.list[[1]],
+          p.list[[2]],
+          p.list[[3]],
+          p.list[[4]],
+          p.list[[5]],
+          common.legend = TRUE,
+          nrow=1)
+
+ggsave(p, filename="../plots/cycling_pvl_gbm.png",
+       width=13, height=3.92, units="in")
 
 Vu <- readRDS("../data/aprUMAP.RDS")
 
